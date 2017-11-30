89654c634b9d5e82ffb50788b5aeafc12b41bcdb
youngJin Jun <joj0919@naver.com>
2017-11-30 19:55:32 +0900

    ...

---
+++ javascripts/interest.js
@@ -0,0 +1,22 @@
+function changeSelect(interest){
+    var div = interest.parentNode;
+    var a=0;
+    
+    for(var i = 0 ; i < div.getElementsByTagName('input').length ; i++)
+        div.getElementsByTagName('input')[i].setAttribute("class", "btn_interest");
+    interest.setAttribute("class", "selected_interest");
+
+}
+function next(){
+    var div = document.getElementsByClassName('interest-form')[0];
+    var a = location.href;
+    if(div.getElementsByClassName('selected_interest').length>=5)
+    {
+        if(a==sign_up_interest2.html)
+            location.href='sign_up_interest3.html';
+    }
+    else
+    {
+        alert('모두 선택해주세요');
+    }
+}
